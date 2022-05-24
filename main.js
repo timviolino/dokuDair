f3febf9b762e3f50d2a2f37fce42a6b869d073f7
timviolino <timviolino@gmail.com>
2022-05-24 12:34:30 -0400

    Add data set of 3 million puzzles with ratings

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-var board = document.getElementsByClassName("cell");
+var board = getBoard();
 for (let i = 0; i < board.length; i++) {
   input = board[i].getElementsByTagName("input")[0];
   input.setAttribute("maxlength", "1");
   input.setAttribute("oninput", "this.value=this.value.replace(/[^1-9]/g,'');");
 }
 
-var clearBtn = document.getElementById("clear-btn")
+var clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener('click', clearBoard);
+
+function getBoard() {
+  return document.getElementsByClassName("cell");
+}
 
 function clearBoard() {
-  var board = document.getElementsByClassName("cell");
+  var board = getBoard();
   for (let i = 0; i < board.length; i++) {
     input = board[i].getElementsByTagName("input")[0];
     input.value = "";
   }
 }
-
-clearBtn.addEventListener('click', clearBoard);
-
